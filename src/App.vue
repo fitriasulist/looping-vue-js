134a2febc7c8e0fca62f49df25fbc7f33c7a7cfe
fitriasulist <fitriasulist@gmail.com>
2023-12-01 13:33:35 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <NumberList />
+  </div>
+</template>
+
+<script>
+import NumberList from "@/components/NumberList.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    NumberList,
+  },
+};
+</script>
---
+++ src/components/NumberList.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <h1>List angka 1 sampai 50</h1>
+      <ul>
+        <li v-for="number in numbers" :key="number">{{ number }}</li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        numbers: [],
+      };
+    },
+    created() {
+      this.generateNumbers();
+    },
+    methods: {
+      generateNumbers() {
+        for (let i = 1; i <= 50; i++) {
+          this.numbers.push(i);
+        }
+      },
+    },
+  };
+  </script>
+  
